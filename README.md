2b9e225f1b1f12a668f28c107e2fe0bffdd94941
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2023-04-16 23:30:33 -0700

    converted the code to make it write in svg form

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A customizable tech tree created from JS
 see "example code" for basic utilization.
 
 The goal here is to go from the basic stuff to make something more dynamic (maybe using a json file for configuration purposes)
+
+(note: running the .json file will update the svg, which will show below:)
+
+![alt text](template.svg)
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 The objective of this code is to read from a .json file and use it to make a useable tech tree.
 */
 
-// import nodes from "./nodes.json" assert { type: 'JSON'};
-
+const fs = require('fs');
+const data = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"800\" height=\"500\">\n<foreignObject class=\"node\" x=\"46\" y=\"22\" width=\"800\" height=\"500\">\n<body xmlns=\"http://www.w3.org/1999/xhtml\">\n<style type=\"text/css\">\n/* Copied from the example code */\n* {margin: 0; padding: 0;}\n\n.tree ul {\npadding-top: 20px; position: relative;\ntext-align: center;\ntransition: all 0.5s;\n-webkit-transition: all 0.5s;\n-moz-transition: all 0.5s;\n}\n\n.tree li {\nfloat: left; \ntext-align: center;\nlist-style-type: none;\nposition: relative;\npadding: 20px 5px 0 5px;\n\ntransition: all 0.5s;\n-webkit-transition: all 0.5s;\n-moz-transition: all 0.5s;\n}\n\n/*We will use ::before and ::after to draw the connectors*/\n\n.tree li::before, .tree li::after{\ncontent: '';\nposition: absolute; top: 0; right: 50%;\nborder-top: 1px solid #ccc;\nwidth: 50%; height: 20px;\n}\n.tree li::after{\nright: auto; left: 50%;\nborder-left: 1px solid #ccc;\n}\n\n/*We need to remove left-right connectors from elements without \nany siblings*/\n.tree li:only-child::after, .tree li:only-child::before {\ndisplay: none;\n}\n\n/*Remove space from the top of single children*/\n.tree li:only-child{ padding-top: 0;}\n\n/*Remove left connector from first child and \nright connector from last child*/\n.tree li:first-child::before, .tree li:last-child::after{\nborder: 0 none;\n}\n/*Adding back the vertical connector to the last nodes*/\n.tree li:last-child::before{\nborder-right: 1px solid #ccc;\nborder-radius: 0 5px 0 0;\n-webkit-border-radius: 0 5px 0 0;\n-moz-border-radius: 0 5px 0 0;\n}\n.tree li:first-child::after{\nborder-radius: 5px 0 0 0;\n-webkit-border-radius: 5px 0 0 0;\n-moz-border-radius: 5px 0 0 0;\n}\n\n/*Time to add downward connectors from parents*/\n.tree ul ul::before{\ncontent: '';\nposition: absolute; top: 0; left: 50%;\nborder-left: 1px solid #ccc;\nwidth: 0; height: 20px;\n}\n\n.tree li a{\nborder: 1px solid rgb(204, 204, 204);\npadding: 5px 10px;\ntext-decoration: none;\ntext-align: center;\ncolor: #666;\nfont-family: arial, verdana, tahoma;\nfont-size: 11px;\ndisplay: inline-block;\n\nborder-radius: 5px;\n-webkit-border-radius: 5px;\n-moz-border-radius: 5px;\n\ntransition: all 0.5s;\n-webkit-transition: all 0.5s;\n-moz-transition: all 0.5s;\n\nwidth: 100px;\n}\n\n/*Time for some hover effects*/\n.tree li a:hover {\nbackground: rgb(195, 195, 195); color: #000; border: 1px solid #94a0b4;\n}\n/*Connector styles on hover*/\n.tree li a:hover+ul li::after, \n.tree li a:hover+ul li::before, \n.tree li a:hover+ul::before, \n.tree li a:hover+ul ul::before{\nborder-color:  #94a0b4;\n}\n\n/* time to add my own shit*/\n\n.tree li a.avail {\nbackground-color: yellowgreen;\nfont-weight: bold;\n}\n\n.tree li a.avail:hover {\nbackground-color: rgb(194, 255, 100);\n}\n\n\n.tree li a.complete {\nbackground-color: black ;\ncolor: lightgray;\ntext-decoration: line-through;\n}\n\n.tree li a.complete:hover {\nbackground-color: rgb(70, 70, 70);\n}\n\n/*Thats all. I hope you enjoyed it.\nThanks :)*/            \n</style>\n<div id=\"tech-tree\" class=\"tree\">\n";
 function convertListOfNodes(nodes, avail=true) {
     var html = "<ul>\n"
     for (var i = 0; i < nodes.length; i++) {
@@ -14,7 +14,7 @@ function convertListOfNodes(nodes, avail=true) {
 }
 
 function convertNodeToHtml(node, avail) {
-    var html = "<li>\n" + writeNode(node["content"], node["link"], avail, node["complete"], node["classes"]) + "\n";
+    var html = "<li>\n" + writeNode(node["content"], node["link"], avail, node["complete"], node["classes"]);
 
     if (node["children"].length) html += convertListOfNodes(node["children"], node["complete"]);
 
@@ -23,7 +23,7 @@ function convertNodeToHtml(node, avail) {
 }
 
 function writeNode(content, link, avail, complete, classes) {
-    var element = "\n<a";
+    var element = "<a";
 
     element += " href=\"" + link + "\"";
 
@@ -44,12 +44,15 @@ function writeNode(content, link, avail, complete, classes) {
     return element;
 }
 
+async function writeSVG() {
+    var svg = data;
+    nodes = require('./nodes.json');
+    svg += convertListOfNodes(nodes);
+    svg += '</div></body></foreignObject></svg>';
 
-// console.log(nodes);
+    fs.writeFileSync("svg.svg", svg);
+    return svg;
+}
 
-fetch('nodes.json')
-    .then((response) => response.json())
-    .then((nodes) => document.getElementById("tech-tree").innerHTML = (convertListOfNodes(nodes)))
-
-
+writeSVG();
 // document.getElementById("tech-tree").innerHTML = convertListOfNodes(nodes);
